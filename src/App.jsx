79500e3bc16b7d80e3cd1645fff2bb80c2648160
prajnaaa14 <prajnapsr2005@gmail.com>
2026-05-20 20:56:58 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+
+function App() {
+  return (
+    <div style={{textAlign:"center", marginTop:"100px"}}>
+      <h1>CI/CD Pipeline Demo</h1>
+      <h2>Travis CI + Mattermost</h2>
+      <p>Build Automation Successful</p>
+    </div>
+  );
+}
+
+export default App;
+
